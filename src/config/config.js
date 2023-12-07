671676bf21cf6a37b94f66d605adccebd812254d
Chromax-design <chromax.design@gmail.com>
2023-12-07 15:18:33 +0100

    editted

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://holdinghands-server.onrender.com";
+export const BASE_URL = "https://holdinghands-server.onrender.com"
 // export const BASE_URL = "http://localhost:3000";
